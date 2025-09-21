89a17e6c0cef6f52d428dc2408a49c16cc07a5e5
nitkarsh <nitkarshupadhyay08@gmail.com>
2025-09-21 09:44:06 +0530

    fix: use react-router-dom instead of react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Home from "./pages/home/Home.jsx";
 import About from "./pages/about/About.jsx";
 import Portfolio from "./pages/portfolio/Portfolio.jsx";
-import Contact from "./pages/contact/Contact";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import AppLayout from "./components/AppLayout";
+import Contact from "./pages/contact/Contact.jsx"; // .jsx extension add karo
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // <-- FIXED
+import AppLayout from "./components/AppLayout.jsx";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -32,4 +32,5 @@ const App = () => {
   ]);
   return <RouterProvider router={router} />;
 };
+
 export default App;
