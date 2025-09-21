e145c5d09d1b637f811a9672c0bb11154ecae810
nitkarsh <nitkarshupadhyay08@gmail.com>
2025-09-21 09:51:32 +0530

    fix: use react-router-dom instead of react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./pages/home/Home.jsx";
-import About from "./src/pages/about/About.jsx";
+import About from "./pages/about/About.jsx";
 import Portfolio from "./pages/portfolio/Portfolio.jsx";
 import Contact from "./pages/contact/Contact.jsx"; // .jsx extension add karo
 import { createBrowserRouter, RouterProvider } from "react-router-dom"; // <-- FIXED
